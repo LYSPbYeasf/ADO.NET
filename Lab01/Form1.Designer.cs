76ddf053b9b1dcf1cdfbd939f85b5f0da6052df7
LYSPbYeasf <99254444+LYSPbYeasf@users.noreply.github.com>
2022-02-22 10:35:06 +0300

    Add files via upload

---
+++ Lab01/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace DBConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.подключениеКБазеДанныхToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отключитьСоединениеСБазойДанныхToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.списокПодключенийToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.подключениеКБазеДанныхToolStripMenuItem,
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // подключениеКБазеДанныхToolStripMenuItem
+            // 
+            this.подключениеКБазеДанныхToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.отключитьСоединениеСБазойДанныхToolStripMenuItem});
+            this.подключениеКБазеДанныхToolStripMenuItem.Name = "подключениеКБазеДанныхToolStripMenuItem";
+            this.подключениеКБазеДанныхToolStripMenuItem.Size = new System.Drawing.Size(181, 20);
+            this.подключениеКБазеДанныхToolStripMenuItem.Text = "Подключение к Базе Данных ";
+            this.подключениеКБазеДанныхToolStripMenuItem.Click += new System.EventHandler(this.подключениеКБазеДанныхToolStripMenuItem_Click);
+            // 
+            // отключитьСоединениеСБазойДанныхToolStripMenuItem
+            // 
+            this.отключитьСоединениеСБазойДанныхToolStripMenuItem.Name = "отключитьСоединениеСБазойДанныхToolStripMenuItem";
+            this.отключитьСоединениеСБазойДанныхToolStripMenuItem.Size = new System.Drawing.Size(293, 22);
+            this.отключитьСоединениеСБазойДанныхToolStripMenuItem.Text = "Отключить соединение с Базой Данных";
+            this.отключитьСоединениеСБазойДанныхToolStripMenuItem.Click += new System.EventHandler(this.отключитьСоединениеСБазойДанныхToolStripMenuItem_Click);
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.списокПодключенийToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.файлToolStripMenuItem.Text = "Файл ";
+            // 
+            // списокПодключенийToolStripMenuItem
+            // 
+            this.списокПодключенийToolStripMenuItem.Name = "списокПодключенийToolStripMenuItem";
+            this.списокПодключенийToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.списокПодключенийToolStripMenuItem.Text = "Список подключений";
+            this.списокПодключенийToolStripMenuItem.Click += new System.EventHandler(this.списокПодключенийToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem подключениеКБазеДанныхToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отключитьСоединениеСБазойДанныхToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem списокПодключенийToolStripMenuItem;
+    }
+}
+
---
+++ Lab01/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DBConnection
+{
+    public partial class Form1 : Form
+    {
+        
+        //string testConnect = @"Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Northwind;Data Source=(local)";//
+        public Form1()
+        {
+            InitializeComponent();
+            this.connection.StateChange += new StateChangeEventHandler(this.connection_StateChange);
+
+        }
+        private void connection_StateChange(object sender, StateChangeEventArgs e)
+        {
+            подключениеКБазеДанныхToolStripMenuItem.Enabled = (e.CurrentState == ConnectionState.Closed);
+            отключитьСоединениеСБазойДанныхToolStripMenuItem.Enabled = (e.CurrentState == ConnectionState.Open);
+        }
+        static string GetConnectionStringByName(string name)
+        {
+
+            string returnValue = null;
+            ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings[name];
+
+            if (settings != null)
+            {
+                returnValue = settings.ConnectionString;
+            }
+
+            return returnValue;
+        }
+
+        string connectionString = GetConnectionStringByName("DBConnect.NorthwindConnectionString");
+
+        SqlConnection connection = new SqlConnection();
+
+        private void подключениеКБазеДанныхToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.ConnectionString = connectionString;
+                    connection.Open();
+                    MessageBox.Show("Соединение с базой данных выполнено успешно");
+                }
+                else
+                    MessageBox.Show("Соединение с базой данных уже установлено");
+            }            
+
+            catch (Exception Xcp)
+            {                
+                MessageBox.Show(Xcp.Message, "Unexpected Exception",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void отключитьСоединениеСБазойДанныхToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+                MessageBox.Show("Соединение с базой данных закрыто");
+            }
+            else
+                MessageBox.Show("Соединение с базой данных уже закрыто");
+        }
+        
+        private void списокПодключенийToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ConnectionStringSettingsCollection settings = ConfigurationManager.ConnectionStrings;
+
+            if (settings != null)
+            {
+                foreach (ConnectionStringSettings cs in settings)
+                {
+                    MessageBox.Show("name = " + cs.Name);
+                    MessageBox.Show("providerName = " + cs.ProviderName);
+                    MessageBox.Show("connectionString = " + cs.ConnectionString);
+
+                }
+            }
+        }
+    }
+}
